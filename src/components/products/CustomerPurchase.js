5357b96a8670fe402e9be48a211433c20eb353ed
Jajuan Jones <jajuanojones2@gmail.com>
2022-03-07 14:30:25 -0600

    created an aggragate to display eaches of candies purchased

--- src/components/products/CustomerPurchase.js
+++ src/components/products/CustomerPurchase.js
@@ -2,9 +2,12 @@ import React, {useState, useEffect} from "react"
 
 export const CustomerPurchase = () => {
     const [purchases, updatePurchase] = useState([])
+    const [aggragateMap, setMap] = useState([])
 
+    // create a variable to store our customer so we can display their purchases
     const kustomer = localStorage.getItem("kandy_customer")
 
+    // useEffect to update the customer purchases on page load
     useEffect(
         () => {
             fetch(`http://localhost:8088/purchases?customerId=${parseInt(kustomer)}&_expand=product`)
@@ -17,38 +20,54 @@ export const CustomerPurchase = () => {
         },[]
     )
     
+    // useEffect that runs our aggragate Map whenever ever purchases changes for our customer
+    useEffect(
+        () => {
+            createLineItem()
+        },[purchases]
+    )
+
     // create a function that iterates over customer purchase
     const createLineItem = () => {
         // create a blank Map
         const aggragateMap = new Map()
         // iterate over customer purchase
-        for (const map of aggragateMap) {
+        for (const purchase of purchases) {
             // check if object has current product.id as its key
-            if (map) {
+            if (aggragateMap.has(purchase.productId && purchase.product.price)) {
                 // increment the value property by 1
-                map.set("value", (map.get("value")) + 1)
+                aggragateMap.set(purchase.productId, (aggragateMap.get(purchase.productId)) + 1)
+                // aggragateMap.set(purchase.product.price, (aggragateMap.get(purchase.product.price)) + purchase.product.price)
             }
             // if not add a new key/value pair
             else{
-                // key will be an object with the products id and price properties
-                map.set("key", "id", "price")
+                // key will be an object with the products id property
                 // the value will be a quantity
-                map.set("value", 1)
+                aggragateMap.set(purchase.productId, 1)
+                // aggragateMap.set(purchase.product.price, purchase.product.price)
             }
         }
 
+        setMap(aggragateMap)
     }
+    
+    let productShown = []
 
     return (
         <>
             <h2>Your Orders</h2>
             {
                 purchases.map((purchase) => {
-                    return <div>
-                        <h3 key={`purchases--${purchase.id}`}>Order # {purchase.id}</h3>
-                        <p>Quanity {}</p>
-                        <p>1 {purchase.product.name}</p>
-                    </div>
+                    if (!productShown.includes(purchase.productId)) {
+                        productShown.push(purchase.productId)
+                        for (const [productId, purchaseQuantity] of aggragateMap) {
+                            if(productId === purchase.productId) {
+                                return <div>
+                                    <p key={`purchases--${purchase.id}`}>{purchase.product.name} Quantity {purchaseQuantity} {purchase.product.price}</p>
+                                        </div>
+                            }
+                        }
+                    }
                 })
             }
         </>
